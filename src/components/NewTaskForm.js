e39c4777c43a7a7a5293927f938363a6c30d7f1e
Emma Repp <emmarepp.draws@gmail.com>
2022-09-17 21:45:51 -1000

    done

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React,{useState} from "react";
+import CategoryFilter from "./CategoryFilter";
+import NewTaskForm from "./NewTaskForm";
+import TaskList from "./TaskList";
+import { CATEGORIES, TASKS } from "../data";
 
-function NewTaskForm() {
+function App() {
+  const [task, setTask] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+  const [selectedCategoryButton, setSelectedCategoryButton] = useState('All')
+  
+  
+
+  function addNewItemtoList(newItem){
+    setTask([...task,newItem])
+  }
+
+  function deletedItem(deletedItem){
+    setTask(task.filter((item)=>item.text !== deletedItem))
+  }
+ 
+
+  const itemDisplayed = task
+
+  .filter(
+    (item)=>{ 
+    if(selectedCategoryButton==='All') return true
+    return selectedCategoryButton === item.category
+   } 
+   )
+
+  
+          
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter
+        categories={categories}
+        onButton={selectedCategoryButton}
+        selectedButton={setSelectedCategoryButton} 
+      />
+      <NewTaskForm
+        onTaskFormSubmit={addNewItemtoList}
+        categories={categories}
+      />
+      <TaskList 
+        deletedItem={deletedItem}
+        tasks={itemDisplayed} />
+    </div>
   );
 }
 
-export default NewTaskForm;
+export default App;;
